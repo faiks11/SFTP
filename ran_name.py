51d502d796344a5c97ece4891b9b8a5e959f9ff7
faiks11 <faiks482@yandex.ru>
2022-09-13 09:27:03 +0300

    Создание случайного имени

---
+++ ran_name.py
@@ -0,0 +1,7 @@
+import random
+import string
+
+def generate_random_string(length):
+    letters = string.ascii_lowercase
+    rand_string = ''.join(random.choice(letters) for i in range(length))
+    return rand_string
